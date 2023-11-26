ee6cff23714d465dd13e7bfd0466ea931e6a15fa
Muhammad Roisul Adib <99470918+roisuladib@users.noreply.github.com>
2023-11-26 17:44:49 +0700

    change: .gitignore file

--- dist/helpers.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="node" />
-import { BinaryLike } from 'crypto';
-export declare function hash(data: BinaryLike): string;
-export declare function isHashProofed({ hash, difficulty, prefix }: {
-    hash: string;
-    difficulty?: number;
-    prefix?: string;
-}): boolean;
-//# sourceMappingURL=helpers.d.ts.map
--- dist/types.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-// export type DataType<T> = {
-// 	key: string
-// 	value: T
-// }
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=types.js.map
